218c03fe09a8ee25fbe9072228934ef4c3948ebd
Yoshi <hiryu1974@yahoo.co.jp>
2021-07-17 09:03:58 +0900

    最初の追加

---
+++ src/components/bio.js
@@ -0,0 +1,39 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+//import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Bio = () => {
+
+  return (
+    <div className="bio">
+      <StaticImage
+        className="bio-img"
+        layout="fixed"
+        formats={["AUTO", "WEBP", "AVIF"]}
+        src="../images/on.png"
+        width={50}
+        height={50}
+        quality={95}
+        alt="Profile picture"
+      />
+      {/* {author?.name && (
+        <p>
+          Written by <strong>{author.name}</strong> {author?.summary || null}
+          {` `}
+          <a href={`https://twitter.com/${social?.twitter || ``}`}>
+            You should follow them on Twitter
+          </a>
+        </p>
+      )} */}
+    </div>
+  )
+}
+
+export default Bio
